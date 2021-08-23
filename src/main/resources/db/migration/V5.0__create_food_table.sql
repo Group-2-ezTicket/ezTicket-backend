d1e39861117280eabf6685e4387a771801e4fa0e
martier <martier@oocl.com>
2021-08-23 17:02:39 +0800

    [ST025] fix: altered columns in movies and food

--- src/main/resources/db/migration/V5.0__create_food_table.sql
+++ src/main/resources/db/migration/V5.0__create_food_table.sql
@@ -2,6 +2,6 @@ CREATE TABLE if NOT EXISTS food
 (
     packageId INT NOT NULL auto_increment PRIMARY KEY,
     packageName VARCHAR(50) NOT NULL,
-    packageDetails VARCHAR(50) NOT NULL,
+    packageDetails VARCHAR(1000) NOT NULL,
     packagePrice int NOT NULL
 );
