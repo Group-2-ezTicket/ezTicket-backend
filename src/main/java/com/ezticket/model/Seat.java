0b3a3941c4def015a299db22dac99d7d6522d865
balajma <mark.lorenz.balajadia@oocl.com>
2021-08-25 00:14:28 +0800

    [ST013][Back-end] refactor: create model class for seat

---
+++ src/main/java/com/ezticket/model/Seat.java
@@ -0,0 +1,40 @@
+package com.ezticket.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Seat {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private int scheduleId;
+    private int orderId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getScheduleId() {
+        return scheduleId;
+    }
+
+    public void setScheduleId(int scheduleId) {
+        this.scheduleId = scheduleId;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+}
