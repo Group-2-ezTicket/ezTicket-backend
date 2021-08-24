c3af96b668ef73baaecf95cb41d72ef9db87f801
balajma <mark.lorenz.balajadia@oocl.com>
2021-08-25 00:17:38 +0800

    [ST013][Back-end] refactor: create service class for seat

---
+++ src/main/java/com/ezticket/service/SeatService.java
@@ -0,0 +1,18 @@
+package com.ezticket.service;
+
+import com.ezticket.model.Seat;
+import com.ezticket.repository.SeatRepository;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class SeatService {
+    @Resource
+    private SeatRepository seatRepository;
+
+    private List<Seat> getAllSeats(){
+        return seatRepository.findAll();
+    }
+}
